3ce2afa316ebb105529b16c34c383f8cde1712f4
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-07-21 23:24:48 -0400

    adding layout and index

--- myapp.rb
+++ myapp.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 
 get '/' do
-	'hello world'
+	erb :index, :layout => :layout
+end
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  "Hello #{params['name']}!"
+end
+
+set(:probability) { |value| condition { rand <= value } }
+
+get '/win_a_car', :probability => 0.1 do
+  "You won!"
+end
+
+get '/win_a_car' do
+  "Sorry, you lost."
 end
