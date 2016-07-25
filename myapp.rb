27d74b59f1ef3e868b5184a63852cbceb9357a50
Koty Wong <kotywong@Kotys-MBP.fios-router.home>
2016-07-25 13:21:56 -0400

    learning hash vs mash lol

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'pinterest-api'
 require 'httparty'
+require 'pp'
 
 client = Pinterest::Client.new(ENV['PINTEREST_TEST'])
 
@@ -8,19 +9,30 @@ client = Pinterest::Client.new(ENV['PINTEREST_TEST'])
 set :public_folder, File.dirname(__FILE__) + '/public'
 
 get '/' do
-	url = "https://api.pinterest.com/v1/me/pins/?access_token=" + ENV['PINTEREST_TEST']
-	puts url
+	url = "https://api.pinterest.com/v1/me/boards/?access_token=" + ENV['PINTEREST_TEST']
+	# puts url
 	response = HTTParty.get(url)
-	p response.parsed_response
+	test_data = response.parsed_response
 
+	pp test_data["data"][0]["url"]
 
+	p "=================="
+
+	pp client.get_boards.class
+	# pp client.get_boards.keys
+	# p test_data.data[0]
+
+	# url = "https://api.pinterest.com/v1/me/pins/?access_token=" + ENV['PINTEREST_TEST']
+	# puts url
+	# response = HTTParty.get(url)
+	# p response.parsed_response
 
 	data = client.me.data
 	# p client.get_pins
 	client_boards = client.get_boards.data
 	client_pins = client.get_pins.data
 	# p data
-	# p client
+	# p client_boards
 	p '=============='
 	# p data.first_name
 	p '=============='
